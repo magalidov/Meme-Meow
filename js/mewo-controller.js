4fa615b1afb5c79c899dbc503de6a3e3e01e954c
Idov <magalidov@gmail.com>
2020-06-10 12:33:18 +0300

    Render Meme

--- js/mewo-controller.js
+++ js/mewo-controller.js
@@ -1,7 +1,50 @@
 'use strict'
 console.log('Controller');
 
-function onInit(){
-    
+var gElCanvas;
+var gCtx;
 
+function onInit() {
+    gElCanvas = document.getElementById('my-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    resizeCanvas();
+    // drawImg();
+    var meme = getMeme()
+    renderMeme(meme)
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gElCanvas.width = elContainer.offsetWidth;
+    gElCanvas.height = elContainer.offsetHeight;
+}
+
+function onSetMeme(imgId){
+    setMeme(imgId)
+    var meme = getMeme()
+    renderMeme(meme)
+}
+
+function drawText(text, x, y, size,align, fill, stroke, strokeWidth) {
+    gCtx.font = `${size}px impact`;
+    gCtx.textAlign = `${align}`;
+    gCtx.fillStyle = `${fill}`;
+    gCtx.strokeStyle = `${stroke}`;
+    gCtx.lineWidth = `${strokeWidth}`;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function renderMeme(meme){
+    var elImg = new Image();
+    elImg.src = `./img/${meme.selectedImgId}.jpg`;
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height);
+        for (var i= 0; i < meme.lines.length; i++){
+            console.log('idov');
+            var currLine = meme.lines[i]
+            console.log('currLine.x:', currLine.x)
+            drawText(currLine.txt,currLine.x,currLine.y,currLine.size,currLine.align,currLine.fill,currLine.stroke)
+        }
+    }
 }
--- js/mewo-service.js
+++ js/mewo-service.js
@@ -1,2 +1,63 @@
 'use strict'
 console.log('Service');
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 };
+
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] },
+{ id: 2, url: 'img/2.jpg', keywords: ['happy'] },
+{ id: 3, url: 'img/3.jpg', keywords: ['happy'] },
+{ id: 4, url: 'img/4.jpg', keywords: ['happy'] },
+{ id: 5, url: 'img/5.jpg', keywords: ['happy'] },
+{ id: 6, url: 'img/6.jpg', keywords: ['happy'] },
+{ id: 7, url: 'img/7.jpg', keywords: ['happy'] },
+{ id: 8, url: 'img/8.jpg', keywords: ['happy'] },
+{ id: 9, url: 'img/9.jpg', keywords: ['happy'] },
+{ id: 10, url: 'img/10.jpg', keywords: ['happy'] },
+{ id: 11, url: 'img/11.jpg', keywords: ['happy'] },
+{ id: 12, url: 'img/12.jpg', keywords: ['happy'] },
+{ id: 13, url: 'img/13.jpg', keywords: ['happy'] },
+{ id: 14, url: 'img/14.jpg', keywords: ['happy'] },
+{ id: 15, url: 'img/15.jpg', keywords: ['happy'] },
+{ id: 16, url: 'img/16.jpg', keywords: ['happy'] },
+{ id: 17, url: 'img/17.jpg', keywords: ['happy'] },
+{ id: 18, url: 'img/18.jpg', keywords: ['happy'] }
+];
+
+var gMeme = {
+    selectedImgId: 4,
+    selectedLineIdx: { x: 100, y: 70 },
+    lines: [{ txt: 'Your Joke', size: 60, x: 100, y: 70, align: 'left', fill: 'black', stroke: 'white' }]
+}
+
+
+function setMeme(imgId) {
+    gMeme = {
+        selectedImgId: imgId,
+        selectedLineIdx: { x: 100, y: 70 },
+        lines: [{ txt: 'Your Joke', size: 60, x: 100, y: 70, align: 'left', fill: 'black', stroke: 'white' }]
+    }
+}
+function getMeme(){
+    return gMeme
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
